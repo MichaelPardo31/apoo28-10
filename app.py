39824135799cde41f7783b30274e62980eca7ff4
Michael Pascual Pardo Quiñonez <mpardo878@soyudemedellin.edu.co>
2024-10-29 21:12:00 -0500

    commit 2
    Ya resuelto correctamente

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
 from modelo.datos_meteorologicos import DatosMeteorologicos
 
-def main():
-    archivo_datos = "resources/datos.txt"
-    datos_meteorologicos = DatosMeteorologicos(archivo_datos)
-    temp_promedio, humedad_promedio, presion_promedio, velocidad_promedio, direccion_predominante = datos_meteorologicos.procesar_datos()
-    
-    print("Clima: ")
-    print(f"Temperatura promedio: {temp_promedio:.2f}°C")
-    print(f"Humedad promedio: {humedad_promedio:.2f}%")
-    print(f"Presión promedio: {presion_promedio:.2f} hPa")
-    print(f"Velocidad promedio del viento: {velocidad_promedio:.2f} km/h")
-    print(f"Dirección predominante del viento: {direccion_predominante}")
-
-
-if __name__ == "__main__":
-    main()
+datos_meteorologicos = DatosMeteorologicos("assets/datos.txt")
+print(datos_meteorologicos.procesar_datos())
